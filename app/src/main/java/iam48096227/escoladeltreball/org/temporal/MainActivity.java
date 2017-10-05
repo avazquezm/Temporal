c3fa6564827b037baa8f74f350a466d84d52b93a
avazquezm <albert95vazquez@gmail.com>
2017-10-05 20:55:38 +0200

    Initial commit

---
+++ app/src/main/java/iam48096227/escoladeltreball/org/temporal/MainActivity.java
@@ -0,0 +1,13 @@
+package iam48096227.escoladeltreball.org.temporal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
